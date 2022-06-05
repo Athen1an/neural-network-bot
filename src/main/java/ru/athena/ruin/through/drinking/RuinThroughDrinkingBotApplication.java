1e7395061b9c466da863f2a219d2aa892a9427c1
dikii <dikii@vtb.ru>
2022-06-05 19:40:21 +0300

    first commit

---
+++ src/main/java/ru/athena/ruin/through/drinking/RuinThroughDrinkingBotApplication.java
@@ -0,0 +1,13 @@
+package ru.athena.ruin.through.drinking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuinThroughDrinkingBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuinThroughDrinkingBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/athena/ruin/through/drinking/RuinThroughDrinkingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.athena.ruin.through.drinking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuinThroughDrinkingBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
